d12fcfbf07976200da36e4315fc0df5914b26a29
KIMANH <ank180605@gmail.com>
2026-08-02 17:49:20 +0700

    Fix string quotes and formatting in axiosClient.js

--- admin-ui/src/api/axiosClient.js
+++ admin-ui/src/api/axiosClient.js
@@ -1,14 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('admin_token');
+  const token = localStorage.getItem("admin_token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
